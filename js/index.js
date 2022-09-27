f9aabb55678374c51bd7eb49dd19b8058359114b
hasan-mia <hasanrafi69@gmail.com>
2022-09-27 10:55:12 +0600

    added slick slider

--- js/index.js
+++ js/index.js
@@ -1,8 +1,65 @@
+// =====Trend Slider====
 $('.trend').slick({
-    dots: true,
+    centerMode: false,
+    // centerPadding: '60px',
+    dots: false,
     infinite: true,
+    arrows: true,
     speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true
+    slidesToShow: 4,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                variableWidth: true,
+                centerPadding: '40px',
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                variableWidth: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+
+});
+
+// =====Deal Slider====
+$('.deal').slick({
+    centerMode: false,
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+
 });
